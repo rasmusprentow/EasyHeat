f015f9bf8397c977db61d88dd66a93037f6c7af7
Rasmus Prentow <rpr@neogrid.dk>
2014-11-23 13:15:22 +0100

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name easyHeatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the easyHeatApp
+ */
+angular.module('easyHeatApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
