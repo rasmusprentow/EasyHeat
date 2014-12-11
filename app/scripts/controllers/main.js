a37403319cc3bf4623b5ef05966058683ebd4189
Rasmus Prentow <rpr@neogrid.dk>
2014-12-12 00:11:36 +0100

    Work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,26 +8,59 @@
  * Controller of the easyHeatApp
  */
 angular.module('easyHeatApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.bounds = [15, 27];
     var day = [];
-    //$http.get('http:/localhost:3000/temperatures')
-    $scope.days = [
-      {name : 'weekday', hours : day},
-      {name : 'friday', hours : day},
-      {name : 'saturday',hours : day},
-      {name : 'sunday',hours : day}
 
-    ]
-    $scope.days.forEach(function(item)
-    {
-      var day = [];
-      for(var i = 0; i < 24; i++)
+
+
+    $http.get('http://localhost:3000/temperatures/friday')
+      .then(function (data)
       {
-        day[i] = {index: i, temperature: 20}
-      }
-        item.hours = day;
+
+        console.log(data.data);
+      //  $scope.days[1].hours = data.data.hours;
+
+        $scope.days = [
+          {name : 'weekday', hours : data.data.hours},
+          {name : 'friday', hours : data.data.hours},
+          {name : 'saturday',hours : data.data.hours},
+          {name : 'sunday',hours : data.data.hours}
+
+        ]
+      }, function()
+      {
+        $scope.days = [
+          {name : 'weekday', hours : day},
+          {name : 'friday', hours : day},
+          {name : 'saturday',hours : day},
+          {name : 'sunday',hours : day}
+
+        ]
+
+        $scope.days.forEach(function(item)
+        {
+          var day = [];
+          for(var i = 0; i < 24; i++)
+          {
+            day[i] = {index: i, temperature: 20}
+          }
+          item.hours = day;
+        });
+        console.log("Creating new")
+        $http.post('http://localhost:3000/temperatures/', $scope.days[1]);
       });
 
-    //$scope.watch('days');
+
+
+    $scope.update = function () {
+        console.log("ddd")
+        $http.put('http://localhost:3000/temperatures/friday',$scope.days[1])
+          .then(function (res) {
+            console.log(res)
+          }, function (res) {
+            console.log(res)
+          })
+
+      };
   });
--- app/scripts/directives/tempcontrol.js
+++ app/scripts/directives/tempcontrol.js
@@ -14,29 +14,32 @@ angular.module('easyHeatApp')
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
         //The data for our line
-        var container = d3.select(element[0]).append('svg')
-          .attr("height", 200)
-          .attr("width", '100%');
 
-        var width = element[0].clientWidth - 50;
+        scope.$watch('days', function() {
+          var container = d3.select(element[0]).append('svg')
+            .attr("height", 200)
+            .attr("width", '100%');
 
-
-        var data = scope.days[1].hours;
-
+          var width = element[0].clientWidth - 50;
 
 
-        var scaleX = d3.scale.linear().range([0, width]).domain([0,23]);
-        var scaleY = d3.scale.linear().range([200, 0 ]).domain([15,27]);
+          var data = scope.days[1].hours;
 
-     //   var render = function () {
+
+          var scaleX = d3.scale.linear().range([0, width]).domain([0, 23]);
+          var scaleY = d3.scale.linear().range([200, 0]).domain([15, 27]);
+
+          //   var render = function () {
 
 
           //This is the accessor function we talked about above
           var lineFunction = d3.svg.line()
-            .x(function(d,i) { return scaleX(d.index); })
-            .y(function(d) { return scaleY(d.temperature); })
-
-
+            .x(function (d, i) {
+              return scaleX(d.index);
+            })
+            .y(function (d) {
+              return scaleY(d.temperature);
+            })
 
 
           //The line SVG Path we draw
@@ -52,56 +55,64 @@ angular.module('easyHeatApp')
             .data(data)
 
 
-          var dots =   dotsData.enter()
+          var dots = dotsData.enter()
             .append("circle")
-            .attr("cx", function(d,i) { return scaleX(d.index); })
-            .attr("cy",(function(d) { return scaleY(d.temperature); }))
+            .attr("cx", function (d, i) {
+              return scaleX(d.index);
+            })
+            .attr("cy", (function (d) {
+              return scaleY(d.temperature);
+            }))
             .attr("r", 2)
             .attr("class", "circle")
 
-        var xAxis = d3.svg.axis().scale(scaleX)
-          .orient("bottom").ticks(20);
+          var xAxis = d3.svg.axis().scale(scaleX)
+            .orient("bottom").ticks(20);
 
-        var yAxis = d3.svg.axis().scale(scaleY)
-          .orient("right").ticks(5);
-  //        dotsData.exit().remove();
+          var yAxis = d3.svg.axis().scale(scaleY)
+            .orient("right").ticks(5);
+          //        dotsData.exit().remove();
 
-        container.append("g")
-          .attr("class", "y axis")
-          .call(yAxis);
+          container.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
 
-        container.append("g")
-          .attr("class", "x axis")
-          .call(xAxis);
+          container.append("g")
+            .attr("class", "x axis")
+            .call(xAxis);
 
 
-        // }
+          // }
 
-    //    render();
+          //    render();
 
-        container.on("mousedown", function(e) {
+          container.on("mousedown", function (e) {
 
-          var x = Math.round(scaleX.invert(d3.mouse(this)[0]));
-          var y = Math.round(scaleY.invert(d3.mouse(this)[1]));
-          console.log("Clicked " + x + ", " + y)
-          data[x].temperature = y;
-          //render();
-          //var dotsData = container.selectAll("dots")
-          //  .data(data).enter()
-          container.select(".line")   // change the line
-           // .duration(750)
-            .attr("d", lineFunction(data));
+            var x = Math.round(scaleX.invert(d3.mouse(this)[0]));
+            var y = Math.round(scaleY.invert(d3.mouse(this)[1]));
+            console.log("Clicked " + x + ", " + y)
+            data[x].temperature = y;
+            //render();
+            //var dotsData = container.selectAll("dots")
+            //  .data(data).enter()
+            container.select(".line")   // change the line
+              // .duration(750)
+              .attr("d", lineFunction(data));
 
-          container.selectAll(".circle")   // change the line
-            // .duration(750)
-            .data(data)
-            // //.enter()
-          //  .append("circle")
-            .attr("cx", function(d,i) { return scaleX(d.index); })
-            .attr("cy",(function(d) { return scaleY(d.temperature); }))
+            container.selectAll(".circle")   // change the line
+              // .duration(750)
+              .data(data)
+              // //.enter()
+              //  .append("circle")
+              .attr("cx", function (d, i) {
+                return scaleX(d.index);
+              })
+              .attr("cy", (function (d) {
+                return scaleY(d.temperature);
+              }))
 
+          });
         });
-
 
       }
     };
